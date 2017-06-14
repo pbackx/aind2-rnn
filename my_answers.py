0ed9e1737a73df0b9900209d2c4a9ba21ba44527
Peter Backx <peter.backx@gmail.com>
2017-06-14 20:05:48 +0000

    solved first part

--- my_answers.py
+++ my_answers.py
@@ -13,6 +13,10 @@ def window_transform_series(series,window_size):
     X = []
     y = []
 
+    for i in range(0, len(series)-window_size):
+        X.append(series[i:i+window_size])
+        y.append(series[i+window_size])
+
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,7 +27,17 @@ def window_transform_series(series,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    # TODO: build an RNN to perform regression on our time series input/output data
+    model = Sequential()
+    model.add(LSTM(5, input_shape = (window_size,1)))
+    model.add(Dense(1))
+
+    # build model using keras documentation recommended optimizer initialization
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+    # compile the model
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+    return model
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
